24766155abdeacb1d8f40a97cc8f49a592b6944e
uzeltinger <fabiouz@gmail.com>
2019-02-07 15:56:53 -0300

    to build

--- src/app/components/contacto/contacto.component.ts
+++ src/app/components/contacto/contacto.component.ts
@@ -9,23 +9,25 @@ import { ActivatedRoute, ParamMap } from '@angular/router';
 export class ContactoComponent implements OnInit {
 
   ancla: string;
-  recibidoParams = { 'title': '', 'text': '', 'buttonText': '','sectioclass':'' };
+  recibidoParams = { 'title': '', 'text': '', 'buttonText': '', 'sectioclass': '' };
 
   constructor(private route: ActivatedRoute,
-            private _elementRef: ElementRef) { }
+    private _elementRef: ElementRef) { }
 
   ngOnInit() {
     this.route.params.subscribe((params: ParamMap) => {
       this.ancla = params['ancla'];
       console.log('ancla', this.ancla);
-      if(this.ancla=='recibido'){
+      if (this.ancla == 'recibido') {
         /*/contacto/recibido*/
-            this.recibidoParams.title = "Consulta enviada.";
-            this.recibidoParams.text = "Su consulta ha sido enviada.<br>Nos comunicaremos con ud. a la brevedad.<br>¡Muchas gracias!.";
-            this.recibidoParams.buttonText = "Listo";
-            this._elementRef.nativeElement.querySelector('#open-modal-recibido').click();
+        this.recibidoParams.title = "Consulta enviada.";
+        this.recibidoParams.text = "Su consulta ha sido enviada.<br>Nos comunicaremos con ud. a la brevedad.<br>¡Muchas gracias!.";
+        this.recibidoParams.buttonText = "Listo";
+        this._elementRef.nativeElement.querySelector('#open-modal-recibido').click();
       }
-});
+    });
   }
+  modalRecibidoCloseClicked() {
 
+  }
 }
